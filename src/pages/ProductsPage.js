98b77191b40bbcaf5eca41375e884740972029c2
Drakson Bezerra Santos <drakson_bezerra@virgo.atlantico.net.br>
2023-01-21 20:36:49 -0300

    limpeza

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,34 +1,15 @@
 import { Helmet } from 'react-helmet-async';
-import { useEffect, useState } from 'react';
 // @mui
-import { Container, Stack, Typography, Button } from '@mui/material';
+import { Container, Stack, Typography } from '@mui/material';
 // components
-import { ProductSort, ProductList, ProductFilterSidebar } from '../sections/@dashboard/products';
+import { ProductSort, ProductList } from '../sections/@dashboard/products';
 // mock
 import ShowFormButton from '../components/form/button';
 
 // ----------------------------------------------------------------------
 
 export default function ProductsPage() {
-  const [openFilter, setOpenFilter] = useState(false);
 
-  const handleOpenFilter = () => {
-    setOpenFilter(true);
-  };
-
-  const handleCloseFilter = () => {
-    setOpenFilter(false);
-  };
-
-  // useEffect(() => {
-  //   fetch("http://localhost:5000/products")
-  //   .then((response) => {
-  //     return  response.json()
-  //   }).then((data) => {
-  //     console.log(data)
-  //   })
-
-  // }, [])
 
   return (
     <>
@@ -43,11 +24,6 @@ export default function ProductsPage() {
 
         <Stack direction="row" flexWrap="wrap-reverse" alignItems="center" justifyContent="flex-end" sx={{ mb: 5 }}>
           <Stack direction="row" spacing={1} flexShrink={0} sx={{ my: 1 }}>
-            <ProductFilterSidebar
-              openFilter={openFilter}
-              onOpenFilter={handleOpenFilter}
-              onCloseFilter={handleCloseFilter}
-            />
             <ProductSort />
           </Stack>
           <Stack direction="row" spacing={1} flexShrink={0} sx={{ my: 1 }}>
